0b1358fc82fc66d217444e1ef0739fe620fa1a4c
slandelle <slandelle@gatling.io>
2024-05-29 16:32:42 +0200

    Initial commit

---
+++ foo/src/test/java/computerdatabase/BasicSimulation.java
@@ -0,0 +1,38 @@
+package computerdatabase;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.http;
+
+/**
+ * This sample is based on our official tutorials:
+ * <ul>
+ *   <li><a href="https://gatling.io/docs/gatling/tutorials/quickstart">Gatling quickstart tutorial</a>
+ *   <li><a href="https://gatling.io/docs/gatling/tutorials/advanced">Gatling advanced tutorial</a>
+ * </ul>
+ */
+public class BasicSimulation extends Simulation {
+
+    HttpProtocolBuilder httpProtocol =
+        http.baseUrl("http://172.31.15.11:8000")
+            .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+            .acceptLanguageHeader("en-US,en;q=0.5")
+            .acceptEncodingHeader("gzip, deflate")
+            .userAgentHeader(
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/119.0"
+            );
+
+    ScenarioBuilder users = scenario("Users")
+        .repeat(10000).on(
+            http("root").get("/json/1k.json")
+        );
+
+    {
+        setUp(
+            users.injectOpen(atOnceUsers(1000))
+        ).protocols(httpProtocol);
+    }
+}
